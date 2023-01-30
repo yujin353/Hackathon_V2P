944b229f8d59f52170a793141160e4bbecb8483c
yujin353 <87658036+yujin353@users.noreply.github.com>
2023-01-30 10:10:53 +0900

    관심리뷰 오류 수정

--- src/pages/Mypage/TopMenu/Interest.js
+++ src/pages/Mypage/TopMenu/Interest.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import { useAccessTknRefresh } from "../../../hooks";
-import $, { removeData } from "jquery"
+import $ from "jquery"
+//import $, { removeData } from "jquery"
 
 const Interest = () => {
     const accessTknRefresh = useAccessTknRefresh();
@@ -116,46 +117,50 @@ const Interest = () => {
                     <div className="lst_review line">
                         {
                             reviewList.length!=0?
-                            reviewList.map((v) => {
-                                const review = v.like_review
-                                const product = review.product
-                                const member = review.member
-                                const rand_simil = productFit(member.member_id, product.product_id)
-                                return (
-                                    <div className="col" key={v.like_review.review_id}>
-                                        <div className="lst_prd2">
-                                            <Link to={`/main/products/${product.product_id}`} className="b">
-                                                <div className="thumb"><span className="im" style={{ backgroundImage: `url(${product.product_img_path})` }}></span></div>
-                                                <div className="txt">
-                                                    <p className="t1">{product.brand.brand_name}</p>
-                                                    <p className="t2">{product.product_name}</p>
+                                reviewList.map((v) => {
+                                    const review = v.like_review
+                                    const product = review.product
+                                    const member = review.member
+                                    const rand_simil = productFit(member.member_id, product.product_id)
+                                    return (
+                                        <div className="col" key={v.like_review.review_id}>
+                                            <div className="lst_prd2">
+                                                <Link to={`/main/products/${product.product_id}`} className="b">
+                                                    <div className="thumb"><span className="im" style={{ backgroundImage: `url(${product.product_img_path})` }}></span></div>
+                                                    <div className="txt">
+                                                        <p className="t1">{product.brand.brand_name}</p>
+                                                        <p className="t2">{product.product_name}</p>
+                                                    </div>
+                                                </Link>
+                                            </div>
+                                            <div className="tit">
+                                                <div className="thumb" onClick={() => navigate(`/mykiin/neighbor?id=${review.member.member_id}`)}>
+                                                    <span><img src={require("../../../assets/images/common/img_nomem.jpg")}></img></span>
                                                 </div>
-                                            </Link>
-                                        </div>
-                                        <div className="tit">
-                                            <div className="thumb" onClick={() => navigate(`/mykiin/neighbor?id=${review.member.member_id}`)}>
-                                                <span><img src={require("../../../assets/images/common/img_nomem.jpg")}></img></span>
+                                                <p className="h1" onClick={() => navigate(`/mykiin/neighbor?id=${review.member.member_id}`)}>{review.member.username}</p>
+                                                <p className="t1">피부 유사도 <span className="c-blue">{rand_simil}%</span></p>
+                                                <div className="bar_b"><span style={{ width: `${rand_simil}%` }}></span></div>
+                                                <button type="button" className="btn_favorit" id={review.review_id}
+                                                        name={review.review_id} onClick={() => likeReview(review.review_id)}>
+                                                    <span className="i-set i_favorit">좋아요</span>
+                                                </button>
                                             </div>
-                                            <p className="h1" onClick={() => navigate(`/mykiin/neighbor?id=${review.member.member_id}`)}>{review.member.username}</p>
-                                            <p className="t1">피부 유사도 <span className="c-blue">{rand_simil}%</span></p>
-                                            <div className="bar_b"><span style={{ width: `${rand_simil}%` }}></span></div>
-                                            <button type="button" className="btn_favorit" id={review.review_id}
-                                                name={review.review_id} onClick={() => likeReview(review.review_id)}>
-                                                <span className="i-set i_favorit">좋아요</span>
-                                            </button>
-                                        </div>
-                                        <div className="txt_box1">
-                                            <div className="box">
-                                                <p className="t"><span className={`i-aft ${rating_className[review.rating - 1]} sm`}>{rating_txt[review.rating - 1]}</span></p>
-                                                <p>{review.review_article.article_content}</p>
-                                                <button type="button" onClick={() => reportReview(review.review_id)} className="btr"><span className="i-aft i_report">신고하기</span></button>
+                                            <div className="txt_box1">
+                                                <div className="box">
+                                                    <p className="t"><span className={`i-aft ${rating_className[review.rating - 1]} sm`}>{rating_txt[review.rating - 1]}</span></p>
+                                                    {
+                                                        review.review_article ?
+                                                            <p>{review.review_article.article_content}</p>
+                                                            : null
+                                                    }
+                                                    <button type="button" onClick={() => reportReview(review.review_id)} className="btr"><span className="i-aft i_report">신고하기</span></button>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                )
-                            })
-                            :
-                            <p className="emptyArea" style={{color:"#bfc2ca", fontSize:"18px", textAlign:"center", marginTop:"120px"}}>관심리뷰가 없습니다.</p>
+                                    )
+                                })
+                                :
+                                <p className="emptyArea" style={{color:"#bfc2ca", fontSize:"18px", textAlign:"center", marginTop:"120px"}}>관심리뷰가 없습니다.</p>
                         }
                     </div>
                 </div>
