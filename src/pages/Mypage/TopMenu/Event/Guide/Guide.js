1105fa24d982f3f72996d8d94e30932a7147f8b1
yujin353 <87658036+yujin353@users.noreply.github.com>
2023-02-21 11:59:25 +0900

    이미지 에러 해결(S3 링크 삽입)

--- src/pages/Mypage/TopMenu/Event/Guide/Guide.js
+++ src/pages/Mypage/TopMenu/Event/Guide/Guide.js
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {useRef} from "react";
+import {useNavigate} from "react-router-dom";
 import Slider from "react-slick";
 
 const Guide = () => {
@@ -37,35 +37,98 @@ const Guide = () => {
                             <div className="item">
                                 <div className="area_test">
                                     <div className="guide_img">
-                                        <img src={require("../../../../../assets/images/guideimg/guideimage0.png")} />
+                                        <img className="guide0" alt="guide0" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage0.png" />
                                     </div>
                                 </div>
                             </div>
                             <div className="item">
                                 <div className="area_test">
                                     <div className="guide_img">
-                                        <img src={require("../../../../../assets/images/guideimg/guideimage1.png")} />
+                                        <img className="guide1" alt="guide1" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage1.png" />
                                     </div>
                                 </div>
                             </div>
                             <div className="item">
                                 <div className="area_test">
                                     <div className="guide_img">
-                                        <img src={require("../../../../../assets/images/guideimg/guideimage2.png")} />
+                                        <img className="guide2" alt="guide2" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage2.png" />
                                     </div>
                                 </div>
                             </div>
                             <div className="item">
                                 <div className="area_test">
                                     <div className="guide_img">
-                                        <img src={require("../../../../../assets/images/guideimg/guideimage3.png")} />
+                                        <img className="guide3" alt="guide3" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage3.png" />
                                     </div>
                                 </div>
                             </div>
                             <div className="item">
                                 <div className="area_test">
-                                    <div className="guide_img" onClick={() => navigate("../guide2")}>
-                                        <img src={require("../../../../../assets/images/guideimg/guideimage4.png")} />
+                                    <div className="guide_img">
+                                        <img className="guide4" alt="guide4" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage4.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide5" alt="guide5" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage5.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide6" alt="guide6" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage6.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide7" alt="guide7" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage7.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide8" alt="guide8" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage8.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide9" alt="guide9" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage9.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide10" alt="guide10" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage10.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide11" alt="guide11" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage11.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide12" alt="guide12" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage12.png" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="area_test">
+                                    <div className="guide_img">
+                                        <img className="guide13" alt="guide13" src="https://pinktrano.s3.ap-northeast-2.amazonaws.com/event/guideimage13.png" />
                                     </div>
                                 </div>
                             </div>
